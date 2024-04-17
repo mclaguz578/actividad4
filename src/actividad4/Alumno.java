562748854703717882a8e12a45a472216980b962
mclaguz578 <163828638+mclaguz578@users.noreply.github.com>
2024-04-17 13:32:03 +0200

    Update Alumno.java

--- src/actividad4/Alumno.java
+++ src/actividad4/Alumno.java
@@ -2,12 +2,18 @@ package actividad4;
 
 public class Alumno {
 	
+    private String nombre;
+    private int anioNacimiento;
+    private String dni;
 	private Asignatura asignatura1;
     private Asignatura asignatura2;
     private Asignatura asignatura3;
 
     // Constructor que incluye nombre, año de nacimiento y D.N.I.
-    public Alumno(Asignatura asignatura1, Asignatura asignatura2, Asignatura asignatura3) {
+    public Alumno(String nombre, int anioNacimiento, String dni, Asignatura asignatura1, Asignatura asignatura2, Asignatura asignatura3) {
+        this.nombre = nombre;
+        this.anioNacimiento = anioNacimiento;
+        this.dni = dni;
         this.asignatura1 = asignatura1;
         this.asignatura2 = asignatura2;
         this.asignatura3 = asignatura3;
@@ -29,6 +35,30 @@ public class Alumno {
 
     public Asignatura getAsignatura3() {
         return asignatura3;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioNacimiento() {
+        return anioNacimiento;
+    }
+
+    public void setAnioNacimiento(int anioNacimiento) {
+        this.anioNacimiento = anioNacimiento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 
 }
