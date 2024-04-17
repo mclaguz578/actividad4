b035c6a9ff58978b858d3c5e364d1d3b4f2aa87f
mclaguz578 <163828638+mclaguz578@users.noreply.github.com>
2024-04-17 13:36:18 +0200

    Update Actividad4.java

--- src/actividad4/Actividad4.java
+++ src/actividad4/Actividad4.java
@@ -3,27 +3,21 @@ package actividad4;
 public class Actividad4 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
-        // Crear tres asignaturas con identificadores 1, 2 y 3
+        // Se crean tres asignaturas con identificadores 1, 2 y 3
         Asignatura asignatura1 = new Asignatura(1);
         Asignatura asignatura2 = new Asignatura(2);
         Asignatura asignatura3 = new Asignatura(3);
 
-        // Crear un alumno con las tres asignaturas
-        Alumno alumno = new Alumno(asignatura1, asignatura2, asignatura3);
+        // Creamos un alumno con las tres asignaturas y la información personal
+        Alumno alumno = new Alumno("Marina Clares", 1990, "77777777A", asignatura1, asignatura2, asignatura3);
 
-        // Crear un profesor
         Profesor profesor = new Profesor();
-
-        // Asignar calificaciones aleatorias al alumno por parte del profesor
+        // Asignamos las calificaciones aleatorias al alumno por parte del profesor
         profesor.ponerNotas(alumno);
 
-        // Calcular la media de las calificaciones del alumno
+        // Calculamos y mostramos la media del alumno
         double media = profesor.calcularMedia(alumno);
-
-        // Mostrar la media del alumno por pantalla
-        System.out.println("La media del alumno es: " + media);
+        System.out.println("La media del alumno " + alumno.getNombre() + " es: " + media);
     }
 
 }
